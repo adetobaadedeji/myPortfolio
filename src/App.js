56433e3e039920e63010a3a1b1cff55f0b7f7591
Adetoba Adedeji <adetoba22@gmail.com>
2021-09-23 23:02:55 +0100

    implemented animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import About from './components/About'
 import Contact from './components/Contact'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, useLocation } from 'react-router-dom'
 
 const App = () => {
+	function Content() {
+		
+		const location = useLocation()
+		const [displayLocation, setDisplayLocation] = useState(location)
+		const [transitionStage, setTransistionStage] = useState('fadeIn')
+
+		console.log(location)
+
+		useEffect(() => {
+			if (location !== displayLocation) setTransistionStage('fadeOut')
+		}, [location])
+
+		return (
+			<div
+				className={`animate-${transitionStage}`}
+				onAnimationEnd={() => {
+					if (transitionStage === 'fadeOut') {
+						setTransistionStage('fadeIn')
+						setDisplayLocation(location)
+					}
+				}}
+			>
+				<Switch location={displayLocation}>
+					<Route exact path='/' component={About} />
+					<Route path='/projects' component={Projects} />
+					<Route path='/skills' component={Skills} />
+					<Route path='/contact' component={Contact} />
+				</Switch>
+			</div>
+		)
+	}
+
 	return (
 		<main className='relative lg:h-screen font-mulish text-gray-400 bg-gray-900 body-font overflow-x-hidden'>
 			<Navbar />
-			<Switch>
-				<Route exact path='/' component={About} />
-				<Route path='/projects' component={Projects} />
-				<Route path='/skills' component={Skills} />
-				<Route path='/contact' component={Contact} />
-			</Switch>
+			<Content />
 		</main>
 	)
 }
 
 export default App
+
+// import React from 'react'
+// import About from './components/About'
+// import Contact from './components/Contact'
+// import Navbar from './components/Navbar'
+// import Projects from './components/Projects'
+// import Skills from './components/Skills'
+// import { Switch, Route } from 'react-router-dom'
+
+// const App = () => {
+// 	return (
+// 		<main className='relative lg:h-screen font-mulish text-gray-400 bg-gray-900 body-font overflow-x-hidden'>
+// 			<Navbar />
+// 			<Switch>
+// 				<Route exact path='/' component={About} />
+// 				<Route path='/projects' component={Projects} />
+// 				<Route path='/skills' component={Skills} />
+// 				<Route path='/contact' component={Contact} />
+// 			</Switch>
+// 		</main>
+// 	)
+// }
+
+// export default App
