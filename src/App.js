2a6c601103e2b8161f43b6a00dfaa7e97cf2699d
Adetoba Adedeji <adetoba22@gmail.com>
2021-09-24 02:14:06 +0100

    edited App file

--- src/App.js
+++ src/App.js
@@ -6,35 +6,36 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 import { Switch, Route, useLocation } from 'react-router-dom'
 
+export function Content() {
+	const location = useLocation()
+	const [displayLocation, setDisplayLocation] = useState(location)
+	const [transitionStage, setTransistionStage] = useState('fadeIn')
+
+	useEffect(() => {
+		if (location !== displayLocation) setTransistionStage('fadeOut')
+	}, [location])
+
+	return (
+		<div
+			className={`animate-${transitionStage}`}
+			onAnimationEnd={() => {
+				if (transitionStage === 'fadeOut') {
+					setTransistionStage('fadeIn')
+					setDisplayLocation(location)
+				}
+			}}
+		>
+			<Switch location={displayLocation}>
+				<Route exact path='/' component={About} />
+				<Route path='/projects' component={Projects} />
+				<Route path='/skills' component={Skills} />
+				<Route path='/contact' component={Contact} />
+			</Switch>
+		</div>
+	)
+}
+
 const App = () => {
-	function Content() {
-		const location = useLocation()
-		const [displayLocation, setDisplayLocation] = useState(location)
-		const [transitionStage, setTransistionStage] = useState('fadeIn')
-
-		useEffect(() => {
-			if (location !== displayLocation) setTransistionStage('fadeOut')
-		}, [location])
-
-		return (
-			<div
-				className={`animate-${transitionStage}`}
-				onAnimationEnd={() => {
-					if (transitionStage === 'fadeOut') {
-						setTransistionStage('fadeIn')
-						setDisplayLocation(location)
-					}
-				}}
-			>
-				<Switch location={displayLocation}>
-					<Route exact path='/' component={About} />
-					<Route path='/projects' component={Projects} />
-					<Route path='/skills' component={Skills} />
-					<Route path='/contact' component={Contact} />
-				</Switch>
-			</div>
-		)
-	}
 
 	return (
 		<main className='relative lg:h-screen font-mulish text-gray-400 bg-gray-900 body-font overflow-x-hidden'>
